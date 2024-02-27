a9103ae49216f3c356f6e77d187e57bf043b0af5
garvitkhare <144166803+garvitkhare@users.noreply.github.com>
2024-02-27 23:43:36 +0530

    Update scc.js

--- scc.js
+++ scc.js
@@ -1,11 +1,14 @@
-air= document.querySelector("#i1");
-liq= document.querySelector("#i2");
-async function check(a, b){
-const den= (a/(a-b))*0.825;
-x= ((((1/den)*9.515*19.3)-19.3)/(9.515-19.3))*100;
-document.querySelector(".result").innerHTML= "Purity is "+ x.toFixed(2)+ "%";
+const air = document.querySelector("#i1");
+const liq = document.querySelector("#i2");
+const kk = document.querySelector("#i3");
+
+async function check(a, b, k) {
+    const den = (a / (a - b)) * k;
+    const x = ((((1 / den) * 9.515 * 19.3) - 19.3) / (9.515 - 19.3)) * 100;
+    document.querySelector(".result").innerHTML = "Purity is " + x.toFixed(2) + "%";
 }
+
 const searchbtn = document.querySelector("button");
 searchbtn.addEventListener("click", () => {
-    check(air.value, liq.value);
-})
+    check(parseFloat(air.value), parseFloat(liq.value), parseFloat(kk.value));
+});
